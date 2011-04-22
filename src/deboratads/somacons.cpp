073a1cd1d0a0eaf59bde186b67537be1f17a2989
deboratads <devnull@localhost>
2011-04-22 19:48:50 -0300

    Finalizando solução para somacons.

--- src/deboratads/somacons.cpp
+++ src/deboratads/somacons.cpp
@@ -5,3 +5,21 @@
  *      Author: deborasilva
  */
 
+#include <stdio.h>
+
+int main() {
+	int n;
+	while (1) {
+		scanf("%d", &n);
+		if (!n) break;
+		int nb = 0;
+		while (n) {
+			if (n & 1) {
+				nb++;
+			}
+			n >>= 1;
+		}
+		printf("%s\n", (nb == 1) ? "NAO" : "SIM");
+	}
+	return 0;
+}
